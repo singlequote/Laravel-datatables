7fb2ef2ebd38f308f4ff78bcade06ba31a6528b7
Wimurk <wimurk@users.noreply.github.com>
2019-07-12 10:01:03 +0200

    Fix issue with searchable relations

--- src/Fields/Multiple.php
+++ src/Fields/Multiple.php
@@ -67,6 +67,7 @@ class Multiple extends Field
      */
     public function each(string $column, \Closure $closure)
     {
+        $this->emptyCheck = false;
         foreach($closure() as $field){
             $this->eachFields[] = [
                 "rendered" => $this->getBetweenTags($field->build(), 'script'),
