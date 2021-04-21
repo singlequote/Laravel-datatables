636d33f7a41aafca57ce1eff3255d0b592f95bf3
Wimurk <wimurk@users.noreply.github.com>
2021-04-21 11:16:01 +0200

    Update table.blade.php

--- src/resources/views/table.blade.php
+++ src/resources/views/table.blade.php
@@ -1,6 +1,6 @@
 <div id="{{ $view->tableId }}datatable-filters" class="row datatable-filters"></div>
 <table id="{{ $view->tableId }}" class="laravel-datatable {{ $view->tableClass }}">
-    <thead class="{{ $view->tableHeadClass }}">
+    <thead class="{{ $view->tableHeadClass ?? '' }}">
         <tr>
             @foreach($view->columns as $header)
             <th>{{ isset($view->translate[$header['name']]) ? __($view->translate[$header['name']]) : __($header['name']) }}</th>
