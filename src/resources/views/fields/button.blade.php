9c336388051579cae1e1670fee23a87bc2f201cd
Wim Pruiksma <wim@quotec.nl>
2021-05-03 14:21:35 +0200

    Added data labels to link and buttons

--- src/resources/views/fields/button.blade.php
+++ src/resources/views/fields/button.blade.php
@@ -28,6 +28,14 @@ you can use the variables data, type and row
     @endforeach
     route = `data-route="${url}"`;
     @endif
+    
+    let label = !`{{ $class->label }}`.length ? '' : `{{ $class->label }}`;
+        
+    @if($class->label)
+    if(row.{{ $class->label }}){
+        label = row.{{ $class->label }};
+    }
+    @endif
 
     @if($class->method === 'GET')
     $(document).on('click', `#${id}:not(.prevent)`,  (e) => {
@@ -40,11 +48,11 @@ you can use the variables data, type and row
 
     });
     let template = `
-        <button ${ dataAttributes } title="{{ $class->title['title'] }}" data-toggle="{{ $class->title['toggle'] }}" onclick="{{ $class->onClick }}" type="button" ${route} id="${id}" class="{{ $class->class }}">{{ $class->label }} {!! $class->icon !!}</button>
+        <button ${ dataAttributes } title="{{ $class->title['title'] }}" data-toggle="{{ $class->title['toggle'] }}" onclick="{{ $class->onClick }}" type="button" ${route} id="${id}" class="{{ $class->class }}">${ label } {!! $class->icon !!}</button>
     `;
     @elseif(in_array($class->method, ['POST', 'DELETE', 'PUT', 'PATCH']))
     let template = `
-        <button ${ dataAttributes } title="{{ $class->title['title'] }}" data-toggle="{{ $class->title['toggle'] }}" onclick="{{ $class->onClick != "" ? $class->onClick : '$("#form${id}").submit();' }}" type="button" id="${id}" class="{{ $class->class }}">{{ $class->label }} {!! $class->icon !!}</button>
+        <button ${ dataAttributes } title="{{ $class->title['title'] }}" data-toggle="{{ $class->title['toggle'] }}" onclick="{{ $class->onClick != "" ? $class->onClick : '$("#form${id}").submit();' }}" type="button" id="${id}" class="{{ $class->class }}">${ label } {!! $class->icon !!}</button>
         <form class="laravel-datatable-form-{{ strtolower($class->method) }}" style="display:none;" method="post" id="form${id}" action="${url}">@csrf @method($class->method)</form>
     `;
     @endif
